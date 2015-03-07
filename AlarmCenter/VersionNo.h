b4dfed4ee9f474f119f0e8996277701e860d0a6e
win7_64_en <1281261856@qq.com>
2015-03-07 20:41:13 +0800

    debug mode no static link mfc

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,1016
-#define PRODUCTVER 0,0,2,1016
-#define STRFILEVER "0,0,2,1016\0"
-#define STRPRODUCTVER "0,0,2,1016\0"
+#define FILEVER 0,0,2,1037
+#define PRODUCTVER 0,0,2,1037
+#define STRFILEVER "0,0,2,1037\0"
+#define STRPRODUCTVER "0,0,2,1037\0"
 #pragma message("lalala")
