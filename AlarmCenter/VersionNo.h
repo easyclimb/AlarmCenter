842a4a7866e4960f051c4c1759f5960c00b4957c
Jack-PC-SSD <1281261856@qq.com>
2015-03-11 18:35:52 +0800

    shutting down

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,1102
-#define PRODUCTVER 0,0,2,1102
-#define STRFILEVER "0,0,2,1102\0"
-#define STRPRODUCTVER "0,0,2,1102\0"
+#define FILEVER 0,0,2,1103
+#define PRODUCTVER 0,0,2,1103
+#define STRFILEVER "0,0,2,1103\0"
+#define STRPRODUCTVER "0,0,2,1103\0"
 #pragma message("lalala")
