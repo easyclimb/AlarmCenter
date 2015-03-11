8853d8338105d38d8d0c24c2d18e299fe5843cb7
Jack-PC-SSD <1281261856@qq.com>
2015-03-11 14:18:26 +0800

    inc build no to 0.0.2.1094

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,1092
-#define PRODUCTVER 0,0,2,1092
-#define STRFILEVER "0,0,2,1092\0"
-#define STRPRODUCTVER "0,0,2,1092\0"
+#define FILEVER 0,0,2,1094
+#define PRODUCTVER 0,0,2,1094
+#define STRFILEVER "0,0,2,1094\0"
+#define STRPRODUCTVER "0,0,2,1094\0"
 #pragma message("lalala")
