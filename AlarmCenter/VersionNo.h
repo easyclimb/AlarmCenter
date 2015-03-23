781d3a048277b3ba89b62d184d3fd110997f517c
Jack-PC-SSD <1281261856@qq.com>
2015-03-23 14:00:45 +0800

    hide button help

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,1425
-#define PRODUCTVER 0,0,2,1425
-#define STRFILEVER "0,0,2,1425\0"
-#define STRPRODUCTVER "0,0,2,1425\0"
+#define FILEVER 0,0,2,1426
+#define PRODUCTVER 0,0,2,1426
+#define STRFILEVER "0,0,2,1426\0"
+#define STRPRODUCTVER "0,0,2,1426\0"
 #pragma message("lalala")
