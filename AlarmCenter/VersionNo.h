adbca61c8d1b7e484180bec3ca2ba10fa93a1f64
Jack-PC-SSD <1281261856@qq.com>
2015-03-26 16:53:12 +0800

    手动管理所有singleton class的内存释放（CLog除外）

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,1594
-#define PRODUCTVER 0,0,2,1594
-#define STRFILEVER "0,0,2,1594\0"
-#define STRPRODUCTVER "0,0,2,1594\0"
+#define FILEVER 0,0,2,1595
+#define PRODUCTVER 0,0,2,1595
+#define STRFILEVER "0,0,2,1595\0"
+#define STRPRODUCTVER "0,0,2,1595\0"
 #pragma message("lalala")
