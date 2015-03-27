c1bfd23bd27b4a47d2e8aa348b342a21cea14b1d
Jack-PC-SSD <1281261856@qq.com>
2015-03-27 15:55:21 +0800

    修复关闭主机管理界面后，主机按钮显示区标题与实际分组名称不符的bug

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,1620
-#define PRODUCTVER 0,0,2,1620
-#define STRFILEVER "0,0,2,1620\0"
-#define STRPRODUCTVER "0,0,2,1620\0"
+#define FILEVER 0,0,2,1621
+#define PRODUCTVER 0,0,2,1621
+#define STRFILEVER "0,0,2,1621\0"
+#define STRPRODUCTVER "0,0,2,1621\0"
 #pragma message("lalala")
