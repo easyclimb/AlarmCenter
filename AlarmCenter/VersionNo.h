be4c5961ce4643f68d4c525fdaf96ea8d66cfaca
Jack-PC-SSD <1281261856@qq.com>
2015-04-17 22:23:00 +0800

    1856.打包pdb文件以便远程调试

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,1855
-#define PRODUCTVER 0,0,2,1855
-#define STRFILEVER "0,0,2,1855\0"
-#define STRPRODUCTVER "0,0,2,1855\0"
+#define FILEVER 0,0,2,1856
+#define PRODUCTVER 0,0,2,1856
+#define STRFILEVER "0,0,2,1856\0"
+#define STRPRODUCTVER "0,0,2,1856\0"
 #pragma message("lalala")
