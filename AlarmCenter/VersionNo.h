3939015d638400c0a476ec76e90669da6f7daa02
Jack-PC-SSD <1281261856@qq.com>
2015-04-23 16:10:58 +0800

    2076.收到异常事件对应的恢复事件后消除原异常事件的报警文字

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,2075
-#define PRODUCTVER 0,0,2,2075
-#define STRFILEVER "0,0,2,2075\0"
-#define STRPRODUCTVER "0,0,2,2075\0"
+#define FILEVER 0,0,2,2076
+#define PRODUCTVER 0,0,2,2076
+#define STRFILEVER "0,0,2,2076\0"
+#define STRPRODUCTVER "0,0,2,2076\0"
 #pragma message("lalala")
