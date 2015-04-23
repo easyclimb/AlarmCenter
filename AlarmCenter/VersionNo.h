6cb1e6db5ac46db597047d047cd6200cda742923
Jack-PC-SSD <1281261856@qq.com>
2015-04-23 14:55:42 +0800

    2069.测试版

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,2067
-#define PRODUCTVER 0,0,2,2067
-#define STRFILEVER "0,0,2,2067\0"
-#define STRPRODUCTVER "0,0,2,2067\0"
+#define FILEVER 0,0,2,2069
+#define PRODUCTVER 0,0,2,2069
+#define STRFILEVER "0,0,2,2069\0"
+#define STRPRODUCTVER "0,0,2,2069\0"
 #pragma message("lalala")
