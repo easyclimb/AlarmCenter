a589eb7c914abdd3e793a99b12cc337ef1e00a35
jack_dev_ninghai <1281261856@qq.com>
2015-06-06 18:21:33 +0800

    2924. setup. disable minimize button on history dialog

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,2922
-#define PRODUCTVER 0,0,2,2922
-#define STRFILEVER "0,0,2,2922\0"
-#define STRPRODUCTVER "0,0,2,2922\0"
+#define FILEVER 0,0,2,2924
+#define PRODUCTVER 0,0,2,2924
+#define STRFILEVER "0,0,2,2924\0"
+#define STRPRODUCTVER "0,0,2,2924\0"
 #pragma message("lalala")
