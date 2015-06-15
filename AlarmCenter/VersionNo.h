fdcda754f1cfc4aa19ce521205e5433c16cde6ef
ninghai_2floor_test_win7_64 <1281261856@qq.com>
2015-06-15 15:46:23 +0800

    ready go back to xian

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,3016
-#define PRODUCTVER 0,0,2,3016
-#define STRFILEVER "0,0,2,3016\0"
-#define STRPRODUCTVER "0,0,2,3016\0"
+#define FILEVER 0,0,2,3017
+#define PRODUCTVER 0,0,2,3017
+#define STRFILEVER "0,0,2,3017\0"
+#define STRPRODUCTVER "0,0,2,3017\0"
 #pragma message("lalala")
