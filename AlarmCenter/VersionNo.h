2a3440bae52cf09e081e06341b7f1a7dc7eee862
Jack-PC-SSD-Win7-64-222 <1281261856@qq.com>
2015-06-24 14:29:00 +0800

    3045.update icons

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,3040
-#define PRODUCTVER 0,0,2,3040
-#define STRFILEVER "0,0,2,3040\0"
-#define STRPRODUCTVER "0,0,2,3040\0"
+#define FILEVER 0,0,2,3045
+#define PRODUCTVER 0,0,2,3045
+#define STRFILEVER "0,0,2,3045\0"
+#define STRPRODUCTVER "0,0,2,3045\0"
 #pragma message("lalala")
