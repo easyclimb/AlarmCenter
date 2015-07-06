80a69a051ffde6b6be27152468b559565ff48ab3
Jack-PC-SSD-Win7-64-222 <1281261856@qq.com>
2015-07-06 13:53:13 +0800

    3086. enable seq confirm

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,3085
-#define PRODUCTVER 0,0,2,3085
-#define STRFILEVER "0,0,2,3085\0"
-#define STRPRODUCTVER "0,0,2,3085\0"
+#define FILEVER 0,0,2,3086
+#define PRODUCTVER 0,0,2,3086
+#define STRFILEVER "0,0,2,3086\0"
+#define STRPRODUCTVER "0,0,2,3086\0"
 #pragma message("lalala")
