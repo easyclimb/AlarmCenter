04a4a1a4d77cb587b38b23a7057c318a1d333494
Jack-PC-SSD-Win7-64-222 <1281261856@qq.com>
2015-07-09 21:45:27 +0800

    after clear record, reset its id to start with 1

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,3174
-#define PRODUCTVER 0,0,2,3174
-#define STRFILEVER "0,0,2,3174\0"
-#define STRPRODUCTVER "0,0,2,3174\0"
+#define FILEVER 0,0,2,3175
+#define PRODUCTVER 0,0,2,3175
+#define STRFILEVER "0,0,2,3175\0"
+#define STRPRODUCTVER "0,0,2,3175\0"
 #pragma message("lalala")
