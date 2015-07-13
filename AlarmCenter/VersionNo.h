5d92beef3737adba6d82ec4775e78c53c9d6aa4a
Jack-PC-SSD-Win7-64-222 <1281261856@qq.com>
2015-07-13 23:44:54 +0800

    upgrade submachine sms cfg ok

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,3246
-#define PRODUCTVER 0,0,2,3246
-#define STRFILEVER "0,0,2,3246\0"
-#define STRPRODUCTVER "0,0,2,3246\0"
+#define FILEVER 0,0,2,3249
+#define PRODUCTVER 0,0,2,3249
+#define STRFILEVER "0,0,2,3249\0"
+#define STRPRODUCTVER "0,0,2,3249\0"
 #pragma message("lalala")
