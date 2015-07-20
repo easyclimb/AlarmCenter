009d9025211a361053a1200e34a92181c31355e1
Jack-PC-SSD-Win7-64-222 <1281261856@qq.com>
2015-07-20 17:41:49 +0800

    enable sms

--- AlarmCenter/Gsm.cpp
+++ AlarmCenter/Gsm.cpp
@@ -222,7 +222,6 @@ void CGsm::SendSms(std::string& phone, std::string& content)
 
 void CGsm::SendSms(const CString& wphone, const CString& wcontent)
 {
-	return;
 	SendSmsTask* task = new SendSmsTask();
 	std::string phone(Utf16ToAnsi(wphone));
 	std::string content(Utf16ToAnsi(wcontent));
