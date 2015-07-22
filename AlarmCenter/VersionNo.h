64e776e8a950eb0508fa426d58b32fc2f9b8c288
Jack-PC-SSD-Win7-64-222 <1281261856@qq.com>
2015-07-22 16:02:08 +0800

    baidu map done

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 0,0,2,3443
-#define PRODUCTVER 0,0,2,3443
-#define STRFILEVER "0,0,2,3443\0"
-#define STRPRODUCTVER "0,0,2,3443\0"
+#define FILEVER 0,0,2,3446
+#define PRODUCTVER 0,0,2,3446
+#define STRFILEVER "0,0,2,3446\0"
+#define STRPRODUCTVER "0,0,2,3446\0"
 #pragma message("lalala")
