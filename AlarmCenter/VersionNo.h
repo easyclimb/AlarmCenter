2f38a5d1b079410fc5d02f33b82f68b864262778
win7_64_en <1281261856@qq.com>
2015-09-28 15:48:00 +0800

    donot show 'video device mgr dlg; for a while

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#define FILEVER 1,0,0,4511
-#define PRODUCTVER 1,0,0,4511
-#define STRFILEVER "1,0,0,4511\0"
-#define STRPRODUCTVER "1,0,0,4511\0"
+#define FILEVER 1,0,0,4512
+#define PRODUCTVER 1,0,0,4512
+#define STRFILEVER "1,0,0,4512\0"
+#define STRPRODUCTVER "1,0,0,4512\0"
