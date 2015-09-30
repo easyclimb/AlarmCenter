ed94cc8bbe0f1876d40bedcdc7f1c35c2875ed75
win7_64_en <1281261856@qq.com>
2015-09-30 10:38:55 +0800

    test 11 clients ok

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#define FILEVER 1,0,0,4580
-#define PRODUCTVER 1,0,0,4580
-#define STRFILEVER "1,0,0,4580\0"
-#define STRPRODUCTVER "1,0,0,4580\0"
+#define FILEVER 1,0,0,4581
+#define PRODUCTVER 1,0,0,4581
+#define STRFILEVER "1,0,0,4581\0"
+#define STRPRODUCTVER "1,0,0,4581\0"
