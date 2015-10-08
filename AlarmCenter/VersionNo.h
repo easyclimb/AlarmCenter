c8d4a26bb32ce73656ecc96f4e5985a579624038
win7_64_en <1281261856@qq.com>
2015-10-08 14:59:40 +0800

    ready to ninghai

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#define FILEVER 1,0,5,4760
-#define PRODUCTVER 1,0,5,4760
-#define STRFILEVER "1,0,5,4760\0"
-#define STRPRODUCTVER "1,0,5,4760\0"
+#define FILEVER 1,0,5,4762
+#define PRODUCTVER 1,0,5,4762
+#define STRFILEVER "1,0,5,4762\0"
+#define STRPRODUCTVER "1,0,5,4762\0"
