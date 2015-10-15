41376f3bfdb932090d4b4387b104e56c3b05cd60
hb-dev-3rd-floor-3screen <1281261856@qq.com>
2015-10-15 13:24:04 +0800

    release 1.0.5.4912

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#define FILEVER 1,0,5,4910
-#define PRODUCTVER 1,0,5,4910
-#define STRFILEVER "1,0,5,4910\0"
-#define STRPRODUCTVER "1,0,5,4910\0"
+#define FILEVER 1,0,5,4912
+#define PRODUCTVER 1,0,5,4912
+#define STRFILEVER "1,0,5,4912\0"
+#define STRPRODUCTVER "1,0,5,4912\0"
