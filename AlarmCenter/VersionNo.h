9f41753d54310cffb709949ac1b928a9e3e065ad
hb-dev-3rd-floor-3screen <1281261856@qq.com>
2015-10-19 14:43:21 +0800

    fixed Welcom to Welcome

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#define FILEVER 1,0,5,5081
-#define PRODUCTVER 1,0,5,5081
-#define STRFILEVER "1,0,5,5081\0"
-#define STRPRODUCTVER "1,0,5,5081\0"
+#define FILEVER 1,0,5,5082
+#define PRODUCTVER 1,0,5,5082
+#define STRFILEVER "1,0,5,5082\0"
+#define STRPRODUCTVER "1,0,5,5082\0"
