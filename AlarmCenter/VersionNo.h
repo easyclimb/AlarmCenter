e3ab4f5f7c69aba3109fb79301571da138a124ce
win7_64_en <1281261856@qq.com>
2016-01-07 17:54:41 +0800

    one string two line

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 1,2,1,6608
-#define PRODUCTVER 1,2,1,6608
-#define STRFILEVER "1,2,1,6608\0"
-#define STRPRODUCTVER "1,2,1,6608\0"
+#define FILEVER 1,2,1,6609
+#define PRODUCTVER 1,2,1,6609
+#define STRFILEVER "1,2,1,6609\0"
+#define STRPRODUCTVER "1,2,1,6609\0"
 
