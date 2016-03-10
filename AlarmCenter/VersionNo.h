795f82a6a96310b201588fc36ee0ec5505872c37
mbp-win10-en-64 <1281261856@qq.com>
2016-03-10 13:56:38 +0800

    released 1.2.46.7261

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 1,2,46,7260
-#define PRODUCTVER 1,2,46,7260
-#define STRFILEVER "1,2,46,7260\0"
-#define STRPRODUCTVER "1,2,46,7260\0"
+#define FILEVER 1,2,46,7261
+#define PRODUCTVER 1,2,46,7261
+#define STRFILEVER "1,2,46,7261\0"
+#define STRPRODUCTVER "1,2,46,7261\0"
 
