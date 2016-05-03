026ab908416e2103625c56aaa7780871da2aa338
win7_64_en <1281261856@qq.com>
2016-05-03 23:09:14 +0800

    released 1.3.9.8387

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 1,3,8,8386
-#define PRODUCTVER 1,3,8,8386
-#define STRFILEVER "1,3,8,8386\0"
-#define STRPRODUCTVER "1,3,8,8386\0"
+#define FILEVER 1,3,9,8387
+#define PRODUCTVER 1,3,9,8387
+#define STRFILEVER "1,3,9,8387\0"
+#define STRPRODUCTVER "1,3,9,8387\0"
 
