8bcec4d7e5b5f24cf3564640673d5de6d17b1775
win7_64_en <1281261856@qq.com>
2016-06-02 09:21:51 +0800

    ready to code jovision

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 1,4,0,9302
-#define PRODUCTVER 1,4,0,9302
-#define STRFILEVER "1,4,0,9302\0"
-#define STRPRODUCTVER "1,4,0,9302\0"
+#define FILEVER 1,4,0,9312
+#define PRODUCTVER 1,4,0,9312
+#define STRFILEVER "1,4,0,9312\0"
+#define STRPRODUCTVER "1,4,0,9312\0"
 
