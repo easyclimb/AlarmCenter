b765c0abaf7aa49690f603ed7e2ccf37b3483c6f
win7_64_en <1281261856@qq.com>
2016-06-13 16:29:38 +0800

    test stop jovision video also ok

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 1,4,0,9649
-#define PRODUCTVER 1,4,0,9649
-#define STRFILEVER "1,4,0,9649\0"
-#define STRPRODUCTVER "1,4,0,9649\0"
+#define FILEVER 1,4,0,9650
+#define PRODUCTVER 1,4,0,9650
+#define STRFILEVER "1,4,0,9650\0"
+#define STRPRODUCTVER "1,4,0,9650\0"
 
