19aa02cf05d23e1e2bc9d255a06b2ce20fb9a00c
win7_64_en <1281261856@qq.com>
2016-07-01 16:51:03 +0800

    save

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
-#define FILEVER 1,4,8,9950
-#define PRODUCTVER 1,4,8,9950
-#define STRFILEVER "1,4,8,9950\0"
-#define STRPRODUCTVER "1,4,8,9950\0"
+#define FILEVER 1,4,8,9952
+#define PRODUCTVER 1,4,8,9952
+#define STRFILEVER "1,4,8,9952\0"
+#define STRPRODUCTVER "1,4,8,9952\0"
 
