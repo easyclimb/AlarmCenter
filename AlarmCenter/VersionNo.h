d17c006a42726e443179615b023d2e6994bf2271
jack-mechrevo <1281261856@qq.com>
2016-11-12 16:25:55 +0800

    ready to refactor alarm_handle

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#define FILEVER 1,5,3,11876
-#define PRODUCTVER 1,5,3,11876
-#define STRFILEVER "1,5,3,11876\0"
-#define STRPRODUCTVER "1,5,3,11876\0"
+#define FILEVER 1,5,3,11877
+#define PRODUCTVER 1,5,3,11877
+#define STRFILEVER "1,5,3,11877\0"
+#define STRPRODUCTVER "1,5,3,11877\0"
