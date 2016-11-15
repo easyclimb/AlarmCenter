8bcce7bba99c4838dfd9a75fbe0be49f32ff2e5c
jack-mechrevo <1281261856@qq.com>
2016-11-15 15:17:32 +0800

    released 1.5.4.11934

--- AlarmCenter/VersionNo.h
+++ AlarmCenter/VersionNo.h
@@ -1,5 +1,5 @@
 ﻿#pragma once
-#define FILEVER 1,5,3,11932
-#define PRODUCTVER 1,5,3,11932
-#define STRFILEVER "1,5,3,11932\0"
-#define STRPRODUCTVER "1,5,3,11932\0"
+#define FILEVER 1,5,4,11934
+#define PRODUCTVER 1,5,4,11934
+#define STRFILEVER "1,5,4,11934\0"
+#define STRPRODUCTVER "1,5,4,11934\0"
